c1ddb9f8dc2e395ca5a75ae20c10704ed1e5fcf5
LimeIvy <raimu10311221@yahoo.co.jp>
2025-05-26 00:53:44 +0900

    モデルの変更

--- mastra/src/mastra/agents/mcpAgent.ts
+++ mastra/src/mastra/agents/mcpAgent.ts
@@ -1,8 +1,17 @@
 import { google } from "@ai-sdk/google";
+import { createOllama } from "ollama-ai-provider";
 import { Agent } from "@mastra/core/agent";
 import { MCPConfiguration } from "@mastra/mcp";
 
 const BRAVE_API_KEY = process.env.BRAVE_API_KEY || "";
+
+const ollama = createOllama({
+  baseURL: "http://localhost:11434/api",
+});
+
+const chat = ollama.chat("llama3-groq-tool-use:8b", {
+  simulateStreaming: true,
+});
 
 const mcp = new MCPConfiguration({
   servers: {
@@ -21,6 +30,7 @@ export const mcpAgent = new Agent({
   name: "MCP Agent",
   instructions: `
       あなたはウェブ検索ができる便利なアシスタントです。
+      すべて日本語で回答してください。
 
       【情報を求められた場合】
       webSearchToolを使用してウェブ検索を実行してください。webSearchToolは以下のパラメータを受け付けます：
@@ -31,6 +41,7 @@ export const mcpAgent = new Agent({
 
       回答は常に簡潔ですが情報量を保つようにしてください。ユーザーの質問に直接関連する情報を優先して提供してください。
   `,
-  model: google("gemini-2.0-flash"),
+  /* model: google("gemini-2.0-flash"),  */
+  model: chat,
   tools: await mcp.getTools(),
 });
